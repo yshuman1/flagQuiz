40463ab4a89df54ec83e2bb8dcffa735f5ea23b9
Yasin Shuman <yshuman@loyalloops.com>
2017-05-09 18:25:39 -0700

    debugging

--- app/src/main/java/io/yasin/flagquiz/MainActivity.java
+++ app/src/main/java/io/yasin/flagquiz/MainActivity.java
@@ -1,6 +1,12 @@
 package io.yasin.flagquiz;
 
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +14,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true;
+    private boolean preferencesChanged = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +32,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false;
+
+        if (phoneDevice)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (preferencesChanged) {
+            MainActivityFragment quizFragment = (MainActivityFragment)
+                getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+                quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+                quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+                quizFragment.resetQuiz();
+                preferencesChanged = false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+        else
+            return false;
     }
 
     @Override
@@ -40,13 +75,39 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+      Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
     }
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            preferencesChanged = true;
+
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+            if (key.equals(CHOICES)) {
+                quizFragment.updateGuessRows(sharedPreferences);
+                quizFragment.resetQuiz();
+            }
+            else if (key.equals(REGIONS)) {
+                Set<String> regions =
+                        sharedPreferences.getStringSet(REGIONS, null);
+
+                if (regions != null && regions.size() > 0) {
+                    quizFragment.updateRegions(sharedPreferences);
+                    quizFragment.resetQuiz();
+                }
+                else {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, regions);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
 }
+
---
+++ app/src/main/java/io/yasin/flagquiz/SettingsActivityFragment.java
@@ -0,0 +1,20 @@
+package io.yasin.flagquiz;
+
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
