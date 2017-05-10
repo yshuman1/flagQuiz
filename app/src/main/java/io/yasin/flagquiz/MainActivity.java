4945cf6313287b4f5769e02c1a16018eed809fe0
Yasin Shuman <yshuman@loyalloops.com>
2017-05-09 20:58:15 -0700

    fixed a line

--- app/src/main/java/io/yasin/flagquiz/MainActivity.java
+++ app/src/main/java/io/yasin/flagquiz/MainActivity.java
@@ -7,11 +7,8 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -81,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
     }
-    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
             preferencesChanged = true;
